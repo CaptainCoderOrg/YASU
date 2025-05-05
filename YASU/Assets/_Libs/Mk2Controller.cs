3df53e4941760eb6c92b74a1310761421d9e66ec
Captain Coder <captaincodersacademy@gmail.com>
2025-05-04 18:38:50 -0700

    chore: Add Mk2

---
+++ YASU/Assets/_Libs/Mk2Controller.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class Mk2Controller : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidBody;
+    [SerializeField] private bool _isActive;
+    [SerializeField] private float _activationDistance = 20;
+    [SerializeField] private ProjectileController _projectile;
+    [SerializeField] private Transform _projectileSpawn;
+    [SerializeField] private Transform[] _projectilePositions;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _move = 0;
+    [SerializeField] private float _speed = 5f;
+    
+
+    void Update()
+    {
+        if(Vector2.Distance(Camera.main.transform.position, transform.position) < _activationDistance && !_isActive)
+        {
+            _isActive = true;
+            StartMove();
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 target = Vector2.MoveTowards(transform.position, transform.position + transform.right, _move*_speed*Time.fixedDeltaTime);       
+        _rigidBody.MovePosition(target);
+    }
+
+    public void StartAttack()
+    {
+        _move = 0;
+        _animator.SetTrigger("Fire");
+    }
+
+    public void PerformAttack()
+    {
+        Debug.Log("Preforming attack!");
+        foreach (Transform t in _projectilePositions)
+        {
+            Debug.Log("Spawning projectil!");
+            ProjectileController p = Instantiate(_projectile);
+            p.FireTowards(_projectileSpawn.position, t.position);
+        }
+    }
+
+    public void StartMove()
+    {
+        _animator.SetTrigger("Move");
+    }
+
+    public void Move(float direction) => _move = direction;
+
+    
+}
